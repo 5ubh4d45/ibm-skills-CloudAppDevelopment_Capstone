3bfdfd44cca57e276ee956033d89f04475166448
Subha Das <connect.subhadas@gmail.com>
2023-07-11 17:37:40 +0530

    updated admin page with better inline model

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,14 +5,26 @@ from djangoapp.models import CarMake, CarModel
 
 
 # Register your models here.
+# admin.site.register(CarMake)
+# admin.site.register(CarModel)
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'dealer_id', 'car_type', 'year']
+    list_filter = ['car_type', 'year']
+    search_fields = ['name', 'dealer_id']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name', 'description']
+    search_fields = ['name']
 
 # Register models here
-
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
