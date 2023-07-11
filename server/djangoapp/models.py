b9aefd839c1fb250b5cdc26735a224ef20765882
Subha Das <connect.subhadas@gmail.com>
2023-07-11 17:10:16 +0530

    implemented CarMake & CarModel

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,14 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(null=False, max_length=400)
+
+    def __str__(self) -> str :
+        return f"Name: {self.name}\
+            \nDescription: {self.description}\
+            \nCar Models: {self.carModels}"
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +27,37 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    dealer_id = models.PositiveIntegerField(null=False)
+    year = models.DateField(null=False)
+    carMake = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE,
+        related_name="carModels",
+        null=False)
+    
+    SEDAN = "SEDAN"
+    SUV = "SUV"
+    WAGON = "WAGON"
+    HATCHBACK = "HATCHBACK"
+    UNKNOWN = "UNDEFINED"
+    Car_Type_Choices = [
+        (SEDAN, "Sedan"),
+        (SUV, "SUV"),
+        (WAGON, "Wagon"),
+        (HATCHBACK, "Hatchback"),
+        (UNKNOWN, "UNKNOWN")
+    ]
+    car_type = models.CharField(null=False, max_length=20, choices=Car_Type_Choices, default=UNKNOWN)
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}\
+            \nDealer ID: {self.dealer_id}\
+            \nYear: {self.year}\
+            \nCar Make: {self.carMake}\
+            \nCar Type: {self.car_type}"
+
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
